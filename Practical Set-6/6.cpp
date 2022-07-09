190e72b517a4597d6d800e1050532204aa1753bf
Akshay Savaliya <108209596+akshay-savaliya@users.noreply.github.com>
2022-07-09 09:04:03 +0530

    Create 6.cpp

---
+++ Practical Set-6/6.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class date
+{
+    int year, month, day;
+
+public:
+    date()
+    {
+        year = 0;
+        month = 0;
+        day = 0;
+    }
+    ~date()
+    {
+    }
+    void getdata()
+    {
+        cout << "Enter a day : ";
+        cin >> day;
+        cout << "Enter a month : ";
+        cin >> month;
+        cout << "Enter a year : ";
+        cin >> year;
+    }
+    void dispdata()
+    {
+        cout << day << " / " << month << " / " << year << endl;
+    }
+    void operator<(date x)
+    {
+        if (year < x.year || year == x.year && month < x.month || year == x.year && month == x.month && day < x.day)
+        {
+            cout << "D1 < D2" << endl;
+        }
+    }
+    void operator>(date x)
+    {
+        if (year > x.year || year == x.year && month > x.month || year == x.year && month == x.month && day > x.day)
+        {
+            cout << "D1 > D2" << endl;
+        }
+    }
+    void operator==(date x)
+    {
+        if (year == x.year && month == x.month && day == x.day)
+        {
+            cout << "D1 = D2" << endl;
+        }
+    }
+    void operator!=(date x)
+    {
+        if (year != x.year || month != x.month || day != x.day)
+        {
+            cout << "D1 != D2" << endl;
+        }
+    }
+};
+
+int main()
+{
+    date d1, d2;
+    cout << "(dd/mm/yy)" << endl;
+    d1.getdata();
+    d2.getdata();
+
+    cout << "D1 : ";
+    d1.dispdata();
+    cout << "D2 : ";
+    d2.dispdata();
+
+    d1 < d2;
+    d1 > d1;
+    d1 == d2;
+    d1 != d2;
+
+    return 0;
+}
